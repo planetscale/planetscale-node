59afcd9539d07e28cb8f2c3b68a59471a4a81000
Nick Van Wiggeren <nick@planetscale.com>
2021-02-05 08:18:58 -0800

    remove commented out code

--- index.js
+++ index.js
@@ -69,31 +69,3 @@ class PSDB {
 }
 
 export default PSDB;
-
-
-/*
-exports.startProxy = function(branch) {
-  var tokenName = process.env.PSDB_TOKEN_NAME
-  var token = process.env.PSDB_TOKEN
-  var dbInfo = process.env.PSDB_DB_NAME.split('/')
-  var runnable = childProcess.spawn(pscalePath, ['--service-token-name', tokenName, '--service-token', token, 'connect', '--org', dbInfo[0], dbInfo[1], branch], {
-    detached: false
-  })
-  
-  runnable.stdout.on('data', function(data) {
-    console.log(data.toString());
-  })
-}
-
-exports.dbPass = function(branch) {
-  var tokenName = process.env.PSDB_TOKEN_NAME
-  var token = process.env.PSDB_TOKEN
-  var dbInfo = process.env.PSDB_DB_NAME.split('/')
-
-  var command = [pscalePath, '--service-token-name', tokenName, '--service-token', token, 'branch', '--org', dbInfo[0], '--json', 'status', dbInfo[1], branch]
-
-  var branchInfo = childProcess.execSync(command.join(' ')).toString()
-  return JSON.parse(branchInfo).password
-}
-
-*/
