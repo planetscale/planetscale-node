6739ff933c715a92b5e724df9a378b55b831bdf9
jtcunning <james@planetscale.com>
2021-07-08 12:00:50 -0700

    switch to templatized build.

---
+++ .buildkite/pipeline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+
+echo "steps:"
+
+for VERSION in 12 14 16; do
+  echo "  - name: \"node:$VERSION build and test %n\""
+  echo "    command: npm ci && npm run build --if-present && npm test"
+  echo "    plugins:"
+  echo "      - docker#v3.8.0:"
+  echo "          image: \"node:$VERSION-slim\""
+done
