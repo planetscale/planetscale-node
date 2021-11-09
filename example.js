50ecc53c806c7bc03ac7a5cdc32757c66e880f07
Enes Ozturk <enesozturk.d@gmail.com>
2021-11-10 00:11:04 +0300

    chore: remove example file

--- example.js
+++
@@ -1,9 +0,0 @@
-const { PSDB } = require('./dist/index.js')
-const conn = new PSDB('main')
-
-async function main() {
-  const [rows, fields] = await conn.execute('select * from likes')
-  console.log(rows, fields)
-}
-
-main()
