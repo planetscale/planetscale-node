c526fbcefc349627681b1c84e5bbc62053f37693
Enes Ozturk <enesozturk.d@gmail.com>
2021-11-09 15:47:01 +0300

    refactor: update main file with created utils

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,13 @@
-import * as https from 'https'
 import * as mysql from 'mysql2'
 import type { Connection, ConnectionOptions } from 'mysql2'
-import type { IncomingMessage } from 'http'
 import { customAlphabet } from 'nanoid'
-import * as crypto from 'crypto'
-import { Crypto as webCryptoPolyfill } from '@peculiar/webcrypto'
-import * as x509 from '@peculiar/x509'
 
-// Define interface for webcrypto. This is not available (yet)
-// in TypeScript for the native webcrypto so we need this to
-// satisfy the rest of the type checks.
-//
-// This definition is the same as what @peculiar/webcrypto uses
-// which functions as a polyfill for Node versions earlier than Node 15
-// which don't provide webcrypto.
-interface WebCrypto {
-  readonly subtle: SubtleCrypto
-  getRandomValues<T extends ArrayBufferView | null>(array: T): T
-}
+import { HttpClient } from './lib/httpClient'
+import { generateKeys } from './lib/webcrypto'
 
-let webcrypto = crypto.webcrypto as unknown as WebCrypto
-// Need to cast here first to unknown and then to WebCrypto since
-// TypeScript doesn't know about WebCrypto as a specific type.
-if (crypto.webcrypto == undefined) {
-  console.warn(
-    'No native webcrypto available, using @peculiar/webcrypto polyfill. Please upgrade to Node 15 or later to avoid this warning.'
-  )
-  webcrypto = new webCryptoPolyfill()
-}
-
-x509.cryptoProvider.set(webcrypto)
 const nanoid = customAlphabet('0123456789abcdefghijklmnopqrstuvwxyz', 12)
-const nodeBtoa = (str: string): string => Buffer.from(str, 'binary').toString('base64')
-const base64encode = typeof btoa !== 'undefined' ? btoa : nodeBtoa
+
+type CertificateData = { id: string; certificate: string; database_branch: { access_host_url: string } }
 
 export class PSDB {
   private branch: string
@@ -44,6 +19,7 @@ export class PSDB {
   private _baseURL: string
   private _headers: { Authorization: string }
   private _connection: Connection | null = null
+  private _httpClient: HttpClient
 
   constructor(branch = 'main', connectionOptions = {}) {
     this.branch = branch
@@ -54,106 +30,59 @@ export class PSDB {
     this._baseURL = 'https://api.planetscale.com'
     this._headers = { Authorization: `${this._tokenname}:${this._token}` }
     this.connectionOptions = connectionOptions
+    this._httpClient = new HttpClient({ headers: this._headers })
   }
 
   async query(data: any, params: any): Promise<any> {
     if (!this._connection) {
       this._connection = await this.createConnection()
     }
+
     return this._connection.promise().query(data, params)
   }
 
   async execute(sql: string, values: any | any[] | { [param: string]: any }): Promise<any> {
     if (!this._connection) {
-      this._connection = await this.createConnection()
+      this._connection = await this._createConnection()
     }
+
     return this._connection.promise().execute(sql, values)
   }
 
-  private async createConnection(): Promise<Connection> {
-    const alg = {
-      name: 'ECDSA',
-      namedCurve: 'P-256',
-      hash: 'SHA-256'
-    }
-
-    const keyPair = await webcrypto.subtle.generateKey(alg, true, ['sign', 'verify'])
-
-    if (!keyPair.privateKey) {
-      throw new Error('Failed to generate keypair')
-    }
-
-    const csr = await x509.Pkcs10CertificateRequestGenerator.create({
-      keys: keyPair,
-      signingAlgorithm: alg
-    })
-
-    const fullURL = new URL(
+  private async _generateCertificate(_certificateString: string): Promise<CertificateData> {
+    const displayName = `pscale-node-${nanoid()}`
+    const requestUrl = new URL(
       `${this._baseURL}/v1/organizations/${this._org}/databases/${this._db}/branches/${this.branch}/certificates`
     )
-
-    const displayName = `pscale-node-${nanoid()}`
-
-    type CertData = { id: string; certificate: string; database_branch: { access_host_url: string } }
-    const { response, body } = await postJSON<CertData>(fullURL, this._headers, {
-      csr: csr.toString(),
+    const { response, body } = await this._httpClient.post<CertificateData>(requestUrl, {
+      csr: _certificateString,
       display_name: displayName
     })
 
-    const status = response.statusCode || 0
+    const status = response.status || 0
     if (status < 200 || status > 299) {
       throw new Error(`HTTP ${status}`)
     }
 
-    const addr = body.database_branch.access_host_url
+    return body
+  }
 
-    const exportPrivateKey = await webcrypto.subtle.exportKey('pkcs8', keyPair.privateKey)
-    const exportedAsString = String.fromCharCode.apply(null, Array.from(new Uint8Array(exportPrivateKey)))
-    const exportedAsBase64 = base64encode(exportedAsString)
-    const pemExported = `-----BEGIN PRIVATE KEY-----\n${exportedAsBase64}\n-----END PRIVATE KEY-----`
+  private async _createConnection(): Promise<Connection> {
+    const { certificateString, sslKey } = await generateKeys()
+    const certificateResponse = await this._generateCertificate(certificateString)
+
+    const addr = certificateResponse.database_branch.access_host_url
 
     return mysql.createConnection({
       ...this.connectionOptions,
-      user: body.id,
+      user: certificateResponse.id,
       host: addr,
       database: this._db,
       ssl: {
-        key: pemExported,
-        cert: body.certificate,
+        key: sslKey,
+        cert: certificateResponse.certificate,
         rejectUnauthorized: true
       }
     })
   }
-}
-
-function postJSON<T>(
-  url: URL,
-  headers: Record<string, string>,
-  body: any
-): Promise<{ response: IncomingMessage; body: T }> {
-  const json = JSON.stringify(body)
-  const options = {
-    hostname: url.host,
-    port: 443,
-    path: url.pathname,
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      'Content-Length': json.length,
-      ...headers
-    }
-  }
-
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, (response) => {
-      let body = ''
-      response.on('data', (chunk) => (body += chunk))
-      response.on('end', () => resolve({ response, body: JSON.parse(body) }))
-    })
-
-    req.on('error', (e) => reject(e))
-    req.write(json)
-    req.end()
-  })
 }
